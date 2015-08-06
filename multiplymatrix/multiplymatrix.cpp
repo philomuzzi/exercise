0032eb4044d8b5e9223ea840b1d4f6f01e8076aa
philomuzzi <gxlee1986@gmail.com>
2015-08-06 11:45:50 +0800

    add multimatrix

---
+++ multiplymatrix/multiplymatrix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+const static int Dimensions = 3;
+
+void multiplyMatrix(const int[] A, const int[] B, int[] C)
+{
+  for (size_t i = 0, j = 0; i < Dimensions, j < Dimensions;) {
+    if (j + 1 % Dimensions == 0) {
+      j = 0;
+      i += 1;
+    } else {
+      j += 1;
+    }
+
+    int value = 0;
+    for (size_t k = 0; k < Dimensions; k++) {
+      value += A[i * Dimensions + k] * B[j + k * Dimensions];
+    }
+
+    C[i * Dimensions + j] = value;
+  }
+}
+
+int int main(int argc, char const *argv[]) {
+  int[] A = {1,2,3,4,5,6,7,8,1};
+  int[] B = {1,2,3,3,2,1,4,5,2};
+  int[9] C;
+  multiplyMatrix(A, B, C);
+  for (int i = 0 ; i < 9; ++i)
+    cout << C[i] << endl;
+  return 0;
+}
